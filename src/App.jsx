f21952694154c2f0b4fbc6ae0fd83a91a0d0dc56
KirYes <kirillkakirill78@gmail.com>
2024-12-19 10:56:15 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kyrylo Yesypenko",
+  title: "IT Student of Politechnika Oposlka",
+  email: "kirillkakirill78@gmail.com",
+  gitHub: "https://github.com/KirYes",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
